bbdc005214456cee34f65bfe91ac32affdb1de56
bog <bog@gozmail.net>
2022-07-12 04:07:52 +0200

    Add: carrousel and movie classes documentation.

--- src/js/carrousel.js
+++ src/js/carrousel.js
@@ -1,75 +1,148 @@
-let ctx = {
-    CarrouselView: class {
-	constructor(movies, prev = null, next = null) {
-	    this.movies = movies;
-	    this._prev = prev;
-	    this._next = next;
+/**
+ * Carrousel manipulation.
+ * @module
+ **/
+
+/**
+ * CarrouselView
+ * @class
+ * Represents a subset of a Carrousel.
+ * @see Carrousel
+ **/    
+class CarrouselView {
+    /**
+     * Create a view.
+     * @constructor
+     * @param {array} movies - The list of the movies hold by the
+     * view.
+     * @param {Movie} prev - The movie preceding the first one of the view if any.
+     * @param {Movie} next - The movie following the last one of the view if any.
+     **/
+    constructor(movies, prev = null, next = null) {
+	this.movies = movies;
+	this._prev = prev;
+	this._next = next;
+    }
+
+    /**
+     * Get the movie preceding the first one if any.
+     * @method
+     * @return {Movie} The previous movie or null.
+     **/
+    prev() {
+	return this._prev;
+    }
+    
+    /**
+     * Get the movie following the last one if any.
+     * @method
+     * @return {Movie} The next movie or null.
+     **/
+    next() {
+	return this._next;
+    }
+}
+
+/**
+ * Represent a carrousel containing movie items.
+ * @class
+ **/
+class Carrousel {
+
+    /**
+     * Create an empty carrousel.
+     * @constructor
+     **/
+    constructor() {
+	this._movies = [];
+	this._position = 0;
+    }
+
+    /**
+     * Returns the current position of the carrousel.
+     * @method
+     * @return {number} The current position of the carrousel.
+     **/
+    get position() { return this._position; }
+    
+    /**
+     * Defines the current position of the carrousel.
+     * @method
+     * @param {number} position - The futur position of the carrousel.
+     **/
+    set position(position) { this._position = position; }
+
+    /**
+     * Request a view of the carrousel.
+     * @method
+     * @param {number} count - The size of the requested view.
+     * @return {CarrouselView} - The requested view.
+     **/
+    request_view(count) {
+	let res = [];
+	const sz = Math.min(this._movies.length, this._position + count);
+	
+	for (let i=this._position; i<sz; i++) {
+	    res.push(this._movies[i]);
 	}
 
-	prev() {
-	    return this._prev;
+	let prev = null;
+	let next = null;
+
+	if (this._position > 0) {
+	    prev = this._movies[this._position - 1];
 	}
 
-	next() {
-	    return this._next;
+	if (sz < this._movies.length) {
+	    next = this._movies[sz];
 	}
-    },
 
-    Carrousel: class {	
-	constructor(size = 7) {
-	    this._movies = [];
+	return new CarrouselView(res, prev, next);
+    }
+
+    /**
+     * Move the carrousel to the left by changing its position.
+     * @method
+     **/
+    move_left() {
+	this._position -= 1;
+
+	if (this._position < 0) {
 	    this._position = 0;
-	}
+	}		
+    }
 
-	get position() { return this._position; }	
-	set position(position) { this._position = position; }
+    /**
+     * Move the carrousel to the right by changing its position.
+     * @method
+     **/
+    move_right() {
+	this._position += 1;
 
-	request_view(count) {
-	    let res = [];
-	    const sz = Math.min(this._movies.length, this._position + count);
-	    
-	    for (let i=this._position; i<sz; i++) {
-		res.push(this._movies[i]);
-	    }
-
-	    let prev = null;
-	    let next = null;
-
-	    if (this._position > 0) {
-		prev = this._movies[this._position - 1];
-	    }
-
-	    if (sz < this._movies.length) {
-		next = this._movies[sz];
-	    }
-
-	    return new ctx.CarrouselView(res, prev, next);
-	}
-	
-	move_left() {
-	    this._position -= 1;
-
-	    if (this._position < 0) {
-		this._position = 0;
-	    }		
-	}
-
-	move_right() {
-	    this._position += 1;
-
-	    if (this._position >= this._movies.length) {
-		this._position = this._movies.length - 1;
-	    }
-	}
-	
-	add(movie) {
-	    this._movies.push(movie);
-	}
-	
-	count() {
-	    return this._movies.length;
+	if (this._position >= this._movies.length) {
+	    this._position = this._movies.length - 1;
 	}
     }
-};
 
-export default ctx;
+    /**
+     * Add a new movie in the carrousel.
+     * @method
+     * @param {Movie} movie - The movie to add to the carrousel.
+     **/
+    add(movie) {
+	this._movies.push(movie);
+    }
+
+    /**
+     * Returns the number of movies.
+     * @return {number} - The number of movies.
+     **/
+    count() {
+	return this._movies.length;
+    }
+}
+
+export default{
+    CarrouselView: CarrouselView,
+    Carrousel: Carrousel
+};
--- src/js/movie.js
+++ src/js/movie.js
@@ -1,8 +1,24 @@
+/**
+ * Movie module.
+ * @module
+ **/
+
+/**
+ * Represent a movie available on "just stream it".
+ * @class
+ **/
+class Movie {
+    /**
+     * Create a new movie.
+     * @constructor
+     * @param {string} title - The title of the movie.
+     **/
+    constructor(title) {
+	this.title = title;
+	this.image_url = 'https://via.placeholder.com/256';
+    }	
+}
+
 export default {
-    Movie: class {
-	constructor(title) {
-	    this.title = title;
-	    this.image_url = 'https://via.placeholder.com/256';
-	}	
-    }
+    Movie: Movie
 }
